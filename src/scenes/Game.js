62284b9a935cba9ea2e7e571050a45676eba6777
bleeccs@gmail.com <ledbrandon@yahoo.com>
2021-01-07 10:02:06 -0800

    Fixing AI behavior

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,40 +1,67 @@
 import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
+    
+    init() {
+        this.paddleRightVelocity = new Phaser.Math.Vector2(0, 0)
+    }
+    
     preload() {
 
     }
 
     create() {
-        const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-        this.physics.add.existing(ball)
-        ball.body.setBounce(1, 1)
-        ball.body.setCollideWorldBounds(true, 1, 1)
-        ball.body. setVelocity(-200, 0)
+        this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+        this.physics.add.existing(this.ball)
+        this.ball.body.setBounce(1, 1)
+        this.ball.body.setCollideWorldBounds(true, 1, 1)
+        this.ball.body. setVelocity(Phaser.Math.Between(-300, 300), Phaser.Math.Between(-300, 300))
 
-        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff)
+        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
         this.physics.add.existing(this.paddleLeft, true)
         
-        const body = this.paddleLeft.body
-
-        body.setMass(100)
-
-        this.physics.add.collider(this.paddleLeft, ball)
+        this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
+        this.physics.add.existing(this.paddleRight, true)
+        
+        this.physics.add.collider(this.paddleLeft, this.ball)
+        this.physics.add.collider(this.paddleRight, this.ball)
 
         this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update() {
         /** @type {Phaser.Physics.Arcade.StaticBody} */
-        this.body = this.paddleLeft.body
+        const body = this.paddleLeft.body
 
         if(this.cursors.up.isDown){
             //console.dir(body) 
             this.paddleLeft.y -= 10
-            this.body.updateFromGameObject() // To move the physics bounce along 
+            body.updateFromGameObject() // To move the physics bounce along 
         } else if (this.cursors.down.isDown) {
             this.paddleLeft.y += 10
             body.updateFromGameObject()
         }
+
+        const diff = this.ball.y - this.paddleRight.y
+
+        if (Math.abs(diff) < 10) {
+            return
+        }
+
+        const aiSpeed = 3
+        if (diff < 0) {
+            this.paddleRightVelocity.y = -aiSpeed
+            if (this.paddleRightVelocity < -10) {
+                this.paddleRightVelocity.y = -10
+            }
+        } else if (diff > 0) {
+            this.paddleRightVelocity.y = aiSpeed
+            if (this.paddleRightVelocity > 10) {
+                this.paddleRightVelocity.y = 10
+            }
+        }
+
+        this.paddleRight.y += this.paddleRightVelocity.y
+        this.paddleRight.body.updateFromGameObject()
     }
 }
